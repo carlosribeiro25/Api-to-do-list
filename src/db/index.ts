1e809b4c205db288a9290fdf4562c6cc106c61f3
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-11 19:15:41 -0300

    refactor: improve logic in index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from 'drizzle-orm/neon-http'
 
-if(!process.env.DATABASE_URL) {
-    throw new Error("Database url precisa ser cetado")
+const databaseUrl = process.env.DATABASE_URL
+
+if(!databaseUrl) {
+    throw new Error("DATABASE_URL precisa ser setada")
 }
 
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(databaseUrl);
