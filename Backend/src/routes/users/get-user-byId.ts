22711b4679a985b2df48ce98e1f3262096de4e50
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-12 09:26:27 -0300

    feat: route to get user by id created

---
+++ Backend/src/routes/users/get-user-byId.ts
@@ -0,0 +1,39 @@
+import { FastifyPluginAsyncZod} from 'fastify-type-provider-zod';
+import z, { coerce } from 'zod';
+import { db } from '../../db/index.js'
+import { users } from '../../db/schema.js';
+import { eq } from 'drizzle-orm';
+
+export const getUserById: FastifyPluginAsyncZod = async (app) =>{
+    app.get('/users/:id', {
+        schema: {
+            params: z.object({
+                id: coerce.number()
+            }),
+            response: {
+                200: z.object({
+                    id: z.coerce.number(),
+                    name: z.string(),
+                    email: z.email(),
+                    password: z.string()
+                }),
+                404: z.object({ error: z.string() })
+            }
+        }
+    }, async (req, reply) => {
+
+        const { id } = req.params 
+
+        const [ getUserById ] =  await db.select()
+        .from(users)
+        .where(eq(users.id, id))
+        
+
+        if(!getUserById) {
+            return reply.status(404).send({ error: 'Usuario não encontrado.'})
+
+        } 
+            return  getUserById
+        
+    })
+}
