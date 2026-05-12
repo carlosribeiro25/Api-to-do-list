1a80e63d537c13657f9adfa6b5aad4c1ab64c45c
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-12 09:15:47 -0300

    feat: route delete a task created

---
+++ Backend/src/routes/tasks/delete-task.ts
@@ -0,0 +1,32 @@
+import { FastifyPluginAsyncZod} from 'fastify-type-provider-zod';
+import z, { coerce } from 'zod';
+import { db } from '../../db/index.js'
+import { tasks } from '../../db/schema.js';
+import { eq } from 'drizzle-orm';
+
+export const deleteTask: FastifyPluginAsyncZod = async (app) =>{
+    app.delete('/tasks/:id', {
+        schema: {
+            params: z.object({
+                id: coerce.number()
+            }),
+            response: {
+                200: z.object({ message: z.string()} ),
+                404: z.object({ error: z.string()} )
+            }
+        }
+    }, async (req, reply) => {
+
+        const { id } = req.params 
+
+        const deleteTask =  await db.delete(tasks)
+        .where(eq(tasks.id, id))
+        .returning()
+
+        if(deleteTask.length >  0) {
+            return reply.status(200).send({ message: 'Tarefa deletada com sucesso.'})
+        } else {
+            return reply.status(404).send({ error: 'Tarefa não encontrada.'})
+        }
+    })
+}
