1162bdeccda9f432331457ca9457b0ef6c200464
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-12 09:22:38 -0300

    feat: add role and created_at fields to users and tasks

--- Backend/src/db/schema.ts
+++ Backend/src/db/schema.ts
@@ -1,10 +1,14 @@
-import { boolean, integer, pgTable, text, time } from "drizzle-orm/pg-core";
+import { boolean, integer, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum('role', ['admin', 'user'])
 
 export const users = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: text().notNull(),
     email: text().unique().notNull(),
-    password: text().notNull()
+    password: text().notNull(),
+    role: roleEnum().default('user').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
 })
 
 export const tasks = pgTable("tasks", {
@@ -13,7 +17,9 @@ export const tasks = pgTable("tasks", {
     date: text(),
     time: text(),
     completed: boolean().default(false).notNull(),
-    userId: integer().references(() => users.id)
-    .notNull()
+    userId: integer().references(() => users.id).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
 })
+
+
 
