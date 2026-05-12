563572a1f0aed7750d1dabb80864eeb47ad9bff0
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-05-11 21:02:20 -0300

    feat: tables migrates to database

---
+++ src/db/schema.ts
@@ -0,0 +1,19 @@
+import {boolean, integer, pgTable, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: text().notNull(),
+    email: text().unique().notNull(),
+    task_id: integer()
+})
+
+export const tasks = pgTable("tasks", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    title: text().notNull(),
+    date: text(),
+    time: text(),
+    completed: boolean().default(false).notNull(),
+    userId: integer().references(() => users.id)
+    .notNull()
+})
+
